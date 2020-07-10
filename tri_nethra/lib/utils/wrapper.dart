efa445b473c2c7e315ebd2c213fbbbf6088d055c
scodes73 <scodes73@gmail.com>
2020-07-11 00:22:38 +0530

    Deleted files from mod2

--- tri_nethra/lib/utils/wrapper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:tri_nethra/screens/authenticate/authenticate.dart';
-
-class Wrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Authenticate();
-  }
-}
